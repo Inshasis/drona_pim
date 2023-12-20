7f4a3c80154e595ddd3d089813874a1d56067030
Inshasis <inshasistechnologies@gmail.com>
2023-12-20 18:15:34 +0000

    add new doctype

--- drona_pim/drona_pim/doctype/colour/colour.js
+++ drona_pim/drona_pim/doctype/colour/colour.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2023, Hidayatali and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Item Category', {
+frappe.ui.form.on('Colour', {
 	// refresh: function(frm) {
 
 	// }
--- drona_pim/drona_pim/doctype/sku/test_sku.py
+++ drona_pim/drona_pim/doctype/sku/test_sku.py
@@ -5,5 +5,5 @@
 from frappe.tests.utils import FrappeTestCase
 
 
-class TestItemCategory(FrappeTestCase):
+class TestSKU(FrappeTestCase):
 	pass
